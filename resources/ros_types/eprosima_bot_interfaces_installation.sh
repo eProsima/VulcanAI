b4f6ebe8a600fb4c03631b3d068c95d42ba3608a
danipiza <dpizarrogallego@gmail.com>
2026-02-06 13:14:14 +0100

    [#24155] Added working tools + openai plans examples
    Signed-off-by: danipiza <dpizarrogallego@gmail.com>

--- resources/ros_types/eprosima_bot_interfaces_installation.sh
+++ resources/ros_types/eprosima_bot_interfaces_installation.sh
@@ -7,3 +7,7 @@ pip install empy       # parsing ROS package.xml files
 pip install catkin-pkg  # IDL parsing
 colcon build --packages-select eprosima_bot_interfaces
 source install/setup.bash
+
+echo -e "\033[32mROS2 Interface EProsimaBotConfig:\033[0m"
+ros2 interface show eprosima_bot_interfaces/msg/EProsimaBotConfig
+
