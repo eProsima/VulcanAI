e825905c2996d54d053aac86c58123cbf0db52da
Carlos Ferreira González <carlosferreira@eprosima.com>
2026-03-18 15:30:44 +0100

    [#23897] Move default node and initialize it
    Signed-off-by: Carlos Ferreira González <carlosferreira@eprosima.com>

--- src/vulcanai/tools/default_tools.py
+++ src/vulcanai/tools/default_tools.py
@@ -20,6 +20,7 @@ It contains atomic tools used to call ROS2 CLI.
 
 import importlib
 import json
+import threading
 import time
 from concurrent.futures import Future
 
@@ -29,10 +30,77 @@ from vulcanai.tools.utils import execute_subprocess, last_output_lines, run_ones
 # ROS2 imports
 try:
     import rclpy
+    from rclpy.node import Node
+    from rclpy.task import Future
 except ImportError:
     raise ImportError("Unable to load default tools because no ROS 2 installation was found.")
 
+
+# This class contains a ROS 2 node that will be loaded if none is provided to launch ROS 2 default tools
+class ROS2DefaultToolNode(Node):
+    def __init__(self, name: str = "vulcanai_ros2_default_tools_node"):
+        if not rclpy.ok():
+            rclpy.init()
+        super().__init__(name)
+        # Dictionary to store created clients
+        self._vulcan_clients = {}
+        # Dictionary to store created publishers
+        self._vulcan_publishers = {}
+
+        # Ensure entities creation is thread-safe.
+        self.node_lock = threading.Lock()
+
+    def get_client(self, srv_type, srv_name):
+        """
+        Get a cached client for the specified service type and name or
+        create a new one if it doesn't exist.
+        """
+        key = (srv_type, srv_name)
+        with self.node_lock:
+            if key not in self._vulcan_clients:
+                client = self.create_client(srv_type, srv_name)
+                self._vulcan_clients[key] = client
+                self.get_logger().info(f"Created new client for {srv_name}")
+            return self._vulcan_clients[key]
+
+    def get_publisher(self, msg_type, topic_name):
+        """
+        Get a cached publisher for the specified message type and topic name or
+        create a new one if it doesn't exist.
+        """
+        key = (msg_type, topic_name)
+        with self.node_lock:
+            if key not in self._vulcan_publishers:
+                publisher = self.create_publisher(msg_type, topic_name, 10)
+                self._vulcan_publishers[key] = publisher
+                self.get_logger().info(f"Created new publisher for {topic_name}")
+            return self._vulcan_publishers[key]
+
+    def wait_for_message(self, msg_type, topic: str, timeout_sec: float = None):
+        """
+        Block until a message is received or timeout expires.
+        Subscriptions are created on demand and destroyed after use to avoid
+        handling spins and callbacks in a separate thread.
+        """
+        future = Future()
+
+        def callback(msg):
+            if not future.done():
+                future.set_result(msg)
+
+        sub = self.create_subscription(msg_type, topic, callback, 10)
+
+        rclpy.spin_until_future_complete(self, future, timeout_sec=timeout_sec)
+        self.destroy_subscription(sub)
+
+        if future.done():
+            return future.result()
+        return None
+
+
 """
+Available ROS 2 CLI commands that can be run with the tools in this file:
+
 - ros2 node
     Commands:
         info  Output information about a node
